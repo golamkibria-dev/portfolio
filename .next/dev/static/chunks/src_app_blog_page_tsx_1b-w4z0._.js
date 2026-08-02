5879de7694d6b2c7d56aa84a91315a9016446b2a
Golam Kibria <golam.kibria.forge@gmail.com>
2026-08-03 01:53:23 +0600

    Remove .next from repository

--- .next/dev/static/chunks/src_app_blog_page_tsx_1b-w4z0._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_1c72fmn._.js","static/chunks/src_components_1c1z6hb._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_layout_tsx_007e4b2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_1fg39zw._.css","static/chunks/src_0v-zis2._.js","static/chunks/node_modules_next_0af92_m._.js","static/chunks/node_modules_framer-motion_dist_es_1o_v_k2._.js","static/chunks/node_modules_motion-dom_dist_es_11kns1k._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0q79z23._.js","static/chunks/node_modules_1ciaxpd._.js"],
-    source: "dynamic"
-});
